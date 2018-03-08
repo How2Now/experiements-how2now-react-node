c42bff936886c7be41ea6fc1615e17266acd7ca6
Andrew Keats <w1663560@my.westminster.ac.uk>
2018-03-08 15:48:17 +0000

    wpi: falinig at including mui css in ssr

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -8,12 +8,18 @@
  */
 
 import React from 'react';
+import { SheetsRegistry } from 'react-jss/lib/jss';
+import JssProvider from 'react-jss/lib/JssProvider';
 import PropTypes from 'prop-types';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 
 // external-global styles must be imported in your JS.
 import normalizeCss from 'normalize.css';
-import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles';
+import {
+  MuiThemeProvider,
+  createMuiTheme,
+  createGenerateClassName,
+} from 'material-ui/styles';
 import s from './Layout.css';
 import appTheme from '../../theme/AppTheme';
 import Header from '../Header';
@@ -21,6 +27,22 @@ import Feedback from '../Feedback';
 import Footer from '../Footer';
 
 const h2nTheme = createMuiTheme(appTheme);
+
+// Create a sheetsRegistry instance.
+const sheetsRegistry = new SheetsRegistry();
+const generateClassName = createGenerateClassName();
+const themeProvider = new MuiThemeProvider({ theme: h2nTheme }, {});
+const jssProvider = new JssProvider(
+  {
+    registry: sheetsRegistry,
+    generateClassName,
+    children: themeProvider,
+  },
+  {},
+);
+
+jssProvider.getChildContext();
+jssProvider.render();
 
 class Layout extends React.Component {
   static propTypes = {
@@ -30,15 +52,28 @@ class Layout extends React.Component {
   render() {
     return (
       <div>
-        <MuiThemeProvider theme={h2nTheme}>
-          <Header />
-          {this.props.children}
-          <Feedback />
-          <Footer />
-        </MuiThemeProvider>
+        <JssProvider
+          registry={sheetsRegistry}
+          generateClassName={generateClassName}
+        >
+          <MuiThemeProvider theme={h2nTheme}>
+            <Header />
+            {this.props.children}
+            <Feedback />
+            <Footer />
+          </MuiThemeProvider>
+        </JssProvider>
       </div>
     );
   }
 }
 
+// Grab the CSS from our sheetsRegistry.
+const muiCss = sheetsRegistry.toString();
+
+/* TODO:
+* Hack an object that has a _getCSS() method that returns the css string
+* **/
+
 export default withStyles(normalizeCss, s)(Layout);
+// export default withStyles(normalizeCss, s, muiCss)(Layout);
