4d845891eaa9e6497a62fecd0e1a8d79bf387617
Andrew Keats <w1663560@my.westminster.ac.uk>
2018-04-04 22:48:06 +0100

    es6 style

--- tools/build-server.js
+++ tools/build-server.js
@@ -1,14 +1,13 @@
-const run = require('./run');
-// import run from './run';
+import run from './run';
 
 console.log('loading deployOnHeroku');
 
 async function deployOnHeroku() {
+  console.log('running deployOnHeroku');
   if (process.env.NODE_ENV == 'production') {
     process.argv.push('--release');
     await run(require('./build'));
   }
 }
 
-// export default deployOnHeroku;
-module.exports = deployOnHeroku;
+export default deployOnHeroku;
