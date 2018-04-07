541fbe06f5f56ad0ac0fc2221d00b606ef42af1d
Andrew Keats <w1663560@my.westminster.ac.uk>
2018-04-07 23:30:18 +0100

    placeholders in for search view

--- src/routes/app/index.js
+++ src/routes/app/index.js
@@ -3,6 +3,10 @@ const appRoutes = [
     path: '/app/dashboard',
     load: () => import(/* webpackChunkName: 'app-dashboard' */ './dashboard'),
   },
+  {
+    path: '/app/search/:type',
+    load: () => import(/* webpackChunkName: 'search' */ './search'),
+  },
 ];
 
 export default appRoutes;
