943b7b0d17ba0205e4a21cb3317917ed42ac3b85
Andrew Keats <w1663560@my.westminster.ac.uk>
2018-04-07 19:21:15 +0100

    wip: getting dashboard tabs in

--- src/routes/app/dashboard/Dashboard.js
+++ src/routes/app/dashboard/Dashboard.js
@@ -8,20 +8,72 @@
  */
 
 import React from 'react';
+import PropTypes from 'prop-types';
+import AppBar from 'material-ui/AppBar';
+import Tabs from 'material-ui/Tabs';
+import Tab from 'material-ui/Tabs/Tab';
+import Typography from 'material-ui/Typography';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './Dashboard.css';
 
-class How2Now extends React.Component {
+function TabContainer(props) {
+  return (
+    <Typography component="div" style={{ padding: 8 * 3 }}>
+      {props.children}
+    </Typography>
+  );
+}
+
+TabContainer.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+class Dashboard extends React.Component {
+  state = {
+    value: 0,
+  };
+
+  handleChange = (event, value) => {
+    this.setState({ value });
+  };
+
+  handleChangeIndex = index => {
+    this.setState({ value: index });
+  };
 
   render() {
+    const { value } = this.state;
+
     return (
       <div className={s.root}>
         <div className={s.container}>
           <h1>Dashboard</h1>
+          <AppBar position="static" color="default">
+            <Tabs
+              value={value}
+              onChange={this.handleChange}
+              indicatorColor="primary"
+              textColor="primary"
+              fullWidth
+            >
+              <Tab label="Account info" />
+              <Tab label="My Tutors" />
+              <Tab label="Progress" />
+              <Tab label="Forum" />
+              <Tab label="Saved resources" />
+              <Tab label="More" />
+            </Tabs>
+          </AppBar>
+          {value === 0 && <TabContainer>Item One</TabContainer>}
+          {value === 1 && <TabContainer>Item Two</TabContainer>}
+          {value === 2 && <TabContainer>Item Three</TabContainer>}
+          {value === 3 && <TabContainer>Item Four</TabContainer>}
+          {value === 4 && <TabContainer>Item Five</TabContainer>}
+          {value === 5 && <TabContainer>Item Six</TabContainer>}
         </div>
       </div>
     );
   }
 }
 
-export default withStyles(s)(How2Now);
+export default withStyles(s)(Dashboard);
